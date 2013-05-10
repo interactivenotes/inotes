676bb9554006bb118cfee122436af5e073599b67
Rafael Nogueira <rafael.nogueira@interactive-tools.de>
2013-05-10 22:51:50 +0200

    Changed to the ModelProvider

---
+++ app/scripts/providers/NoteModel.js
@@ -0,0 +1,66 @@
+(function () {
+	'use strict';
+
+	angular.module('inotesApp')
+		.provider('NoteProvider', function () {
+
+			// localStorage.getItem();
+			// localStorage.setItem();
+
+			//Delete locally stored notes
+			localStorage.clear();
+
+			//Fetch all notes from remote and store locally
+
+			//Build noteKeys (from server list)
+
+			var noteKeys = localStorage.getItem('noteKeys');
+
+			this.$get = function () {
+				return {
+					getNoteList: function () {
+						var
+							ret = [];
+						for (noteKey in noteKeys) {
+							ret.push(JSON.parse(localStorage.getItem(noteKey)));
+						}
+						return ret;
+
+					},
+					getNote: function (noteKey) {
+						return JSON.parse(localStorage.getItem(noteKey));
+					},
+					saveNote: function (note, mode) {
+						mode = mode || 'local';
+
+						//Remote notes are *always' stored remotely and locally
+						//so there's no break!!!
+						switch (mode) {
+							case 'remote':
+								NoteRemoteProvider.saveNote(note);
+							case 'local':
+							default:
+								localStorage.setItem(note.id, JSON.stringify(note));
+						}
+					},
+					deleteNote: function (noteKey, mode) {
+						mode = mode || 'local';
+
+						//Remote notes are *always* deleted remotely and locally
+						//so there's no break!!!
+						switch (mode) {
+							case 'remote':
+								NoteRemoteProvider.deleteNote(noteKey);
+							case 'local':
+							default:
+								localStorage.removeItem(noteKey);
+						}
+
+					},
+					saveDirtyNotes: function(){
+
+					}
+				};
+			};
+		});
+}());
